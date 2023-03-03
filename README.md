d150acfdfdb92429a87439a240f035c91d4d81b9
muthucodes <muthukumarnp1999@gmail.com>
2023-03-03 20:36:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the `form` is submitted if:
+  - Any `input` field is empty. The message for this error should say _"[Field Name] cannot be empty"_
+  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say _"Looks like this is not an email"_
+
+### Screenshot
+
+![](./screenshots/desktop-ss.png)
+![](./screenshots/active-state-ss.png)
+![](./screenshots/mobile-ss.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
---
+++ script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+// Function to select elements
+const selectElement = function (elementIdentifier) {
+  // console.log(elementIdentifier);
+
+  return document.querySelector(elementIdentifier);
+};
+
+// Function to select inputted values
+const obtainValue = function (element) {
+  // console.log(element.value.trim());
+  return element.value.trim();
+};
+
+const firstName = selectElement("#first-name");
+const lastName = selectElement("#last-name");
+const email = selectElement("#email");
+const password = selectElement("#password");
+const btn = selectElement(".btn");
+
+const iconHTML = `<i class="fa-solid fa-circle-exclamation error-icon"></i>`;
+
+const validateInputs = function (element) {
+  if (obtainValue(element) === "") {
+    let div = document.createElement("div");
+    div.className = "error";
+    div.innerText = `${element.getAttribute("placeholder")} cannot be empty`;
+
+    element.style.border = "1px solid red";
+    element.parentElement.after(div);
+    element.insertAdjacentHTML("afterend", iconHTML);
+  }
+};
+
+const validateEmail = function (element) {
+  var emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (obtainValue(element) !== "" && !obtainValue(element).match(emailRegex)) {
+    let div = document.createElement("div");
+    div.className = "error";
+
+    div.innerText = "Looks like this is not an email";
+
+    element.style.border = "1px solid red";
+    element.style.color = "red";
+
+    element.parentElement.after(div);
+    element.insertAdjacentHTML("afterend", iconHTML);
+  }
+};
+
+const resetErrors = function () {
+  document.querySelectorAll(".error").forEach((e) => (e.innerText = ""));
+  document
+    .querySelectorAll("input")
+    .forEach((e) => (e.style.border = "1px solid hsl(246, 25%, 77%)"));
+  document.querySelectorAll(".error-icon").forEach((e) => e.remove());
+};
+
+const init = function () {
+  firstName.value = "";
+  lastName.value = "";
+  email.value = "";
+  password.value = "";
+};
+
+window.onload = function () {
+  // console.log("page loaded");
+  init();
+};
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("submit button clicked");
+  resetErrors();
+  validateInputs(firstName);
+  validateInputs(lastName);
+  validateInputs(password);
+  validateInputs(email);
+  validateEmail(email);
+});
